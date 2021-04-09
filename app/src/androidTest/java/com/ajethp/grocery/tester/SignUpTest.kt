9fd0cfadd1e8f2dd2aa96bae4426736b4c498ce7
claudia <61492986+coloteong@users.noreply.github.com>
2021-04-09 22:04:57 +0800

    claudia@gmail.com

--- app/src/androidTest/java/com/ajethp/grocery/tester/SignUpTest.kt
+++ app/src/androidTest/java/com/ajethp/grocery/tester/SignUpTest.kt
@@ -11,4 +11,73 @@ import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class SignUpTest {
+    private var result = 0
+    @Test
+    fun signupExistingUsername() {
+        val scenario = launch(SignUp::class.java)
+        scenario.onActivity { activity ->
+            result = activity.checkValidLoginDetails(
+                "claudia@gmail.com",
+                "coloteong",
+                "claudia",
+                "claudia"
+            )
+        }
+        assertEquals(result, 5)
+    }
+
+    @Test
+    fun signupPasswordNotMatching() {
+        val scenario = launch(SignUp::class.java)
+        scenario.onActivity { activity ->
+            result = activity.checkValidLoginDetails(
+                "claudia@gmail.com",
+                "cloudy",
+                "claudia1",
+                "claudia"
+            )
+        }
+        assertEquals(result, 4)
+    }
+
+    @Test
+    fun signupEmptyPassword() {
+        val scenario = launch(SignUp::class.java)
+        scenario.onActivity { activity ->
+            result = activity.checkValidLoginDetails("claudia@gmail.com", "coloteong", "", "")
+        }
+        assertEquals(result, 3)
+    }
+
+    @Test
+    fun signupEmptyUsername() {
+        val scenario = launch(SignUp::class.java)
+        scenario.onActivity { activity ->
+            result = activity.checkValidLoginDetails("claudia@gmail.com", "", "claudia", "claudia")
+        }
+        assertEquals(result, 2)
+    }
+
+    @Test
+    fun signupInvalidEmail() {
+        val scenario = launch(SignUp::class.java)
+        scenario.onActivity { activity ->
+            result = activity.checkValidLoginDetails(
+                "claudia.com",
+                "coloteong",
+                "claudia",
+                "claudia"
+            )
+        }
+        assertEquals(result, 1)
+    }
+
+    @Test fun validLoginDetails() {
+        val scenario = launch(SignUp::class.java)
+        scenario.onActivity { activity ->
+            result = activity.checkValidLoginDetails("claudia@gmail.com", "coloteong1", "claudia", "claudia")
+        }
+        assertEquals(result, 6)
+    }
 }
+
